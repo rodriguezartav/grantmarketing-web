e31beaab051ae9d14ef495c36a12de977016de58
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-08 19:45:32 -0600

    fc

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+
+  theme: {
+    extend: {
+      colors: {
+        rose: colors.lime,
+      },
+    },
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
