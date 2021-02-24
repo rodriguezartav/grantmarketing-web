0b4421f000bbec51616a813b7cf5378c90a3fc06
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-23 19:32:54 -0600

    brands

--- pages/login.js
+++ pages/login.js
@@ -17,7 +17,7 @@ export default function LoginPage() {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <Logo />
+            <img src="white_textlogo_dark_background.png" />
             <Login creatable={true} />
           </div>
         </div>
