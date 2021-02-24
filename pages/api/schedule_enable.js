1a15abc5706e5533a152184329d0a0b6c81d1539
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-24 15:18:18 -0600

    brands

--- pages/api/schedule_enable.js
+++ pages/api/schedule_enable.js
@@ -7,13 +7,13 @@ export default async (req, res) => {
       await request
         .put(process.env.API_URL + "/api/schedules/" + req.body.id)
         .set("Accept", "application/json")
-        .auth(req.headers.authorization, { type: "bearer" })
+        .set("authorization", req.headers.authorization)
         .send({ ...req.body, customer_id: parseInt(req.headers.customer_id) });
     else
       await request
         .post(process.env.API_URL + "/api/schedules")
         .set("Accept", "application/json")
-        .auth(req.headers.authorization, { type: "bearer" })
+        .set("authorization", req.headers.authorization)
         .send({
           data: { ...req.body, customer_id: parseInt(req.headers.customer_id) },
         });
