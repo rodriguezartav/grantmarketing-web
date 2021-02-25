33573fc46a4ba93c9f5dd9103a975b53b93c2db4
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-25 08:12:33 -0600

    brands

--- pages/api/create_account.js
+++ pages/api/create_account.js
@@ -6,7 +6,7 @@ export default async (req, res) => {
       .post(process.env.API_URL + "/api/customers")
       .send({ data: { name: req.body.company } })
       .set("Accept", "application/json")
-      .set("authorization", req.headers.authorization);
+      .auth("LINK", { type: "bearer" });
 
     await request
       .post(process.env.API_URL + "/api/users")
@@ -19,13 +19,13 @@ export default async (req, res) => {
         },
       })
       .set("Accept", "application/json")
-      .auth(req.headers.authorization, { type: "bearer" });
+      .auth("LINK", { type: "bearer" });
 
     await request
       .post(process.env.API_URL + "/api/userLogin/getCode")
       .send({ country_code: req.body.countryCode, phone: req.body.phone })
       .set("Accept", "application/json")
-      .set("authorization", req.headers.authorization);
+      .auth("LINK", { type: "bearer" });
 
     return res.status(200).json({});
   } catch (e) {
