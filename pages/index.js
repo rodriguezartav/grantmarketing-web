e09985931c41ce27a36fb08b73a4e479be803625
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-26 19:54:31 -0600

    brands

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
                         </span>
                       </h1>
                       <p className="mt-3 text-2xl text-gray-300 sm:mt-7 sm:text-2xl lg:text-3xl xl:text-3xl">
-                        Order a JD custom program to make it possible!
+                        Order a JD program to make it possible!
                       </p>
 
                       <div className="mt-10 sm:mt-12">
@@ -445,9 +445,268 @@ export default function Home() {
 
             {/* Blog section */}
 
+            {/* This example requires Tailwind CSS v2.0+ */}
+            <div className="bg-white">
+              <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+                <div className="pb-16 xl:flex xl:items-center xl:justify-between">
+                  <div>
+                    <h1 className="text-4xl font-extrabold sm:text-5xl sm:tracking-tight">
+                      <span className="text-gray-900">
+                        Everything you need for
+                      </span>
+                      <span className="text-green-600 ml-1">
+                        {" "}
+                        $2500 a month
+                      </span>
+                    </h1>
+                    <p className="mt-5 text-xl text-gray-500">
+                      Unlimited everything - 15 days trial - 1 month paid trial
+                      - annual contracts there after
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    className="mt-8 w-full bg-green-600 border border-transparent px-5 py-3 inline-flex items-center justify-center text-base font-medium rounded-md text-white hover:bg-green-700 sm:mt-10 sm:w-auto xl:mt-0"
+                  >
+                    Get started today
+                  </a>
+                </div>
+                <div className="border-t border-gray-200 pt-16 xl:grid xl:grid-cols-3 xl:gap-x-8">
+                  <div>
+                    <h2 className="text-base font-semibold text-green-600 tracking-wide uppercase">
+                      Everything you need
+                    </h2>
+                    <p className="mt-2 text-3xl font-extrabold text-gray-900">
+                      All-in-one platform
+                    </p>
+                    <p className="mt-4 text-lg text-gray-500">
+                      JD is a boutique platform that offers integration services
+                      to a handfull of proactive customers.
+                    </p>
+                  </div>
+                  <div className="mt-4 sm:mt-8 md:mt-10 md:grid md:grid-cols-2 md:gap-x-8 xl:mt-0 xl:col-span-2">
+                    <ul className="divide-y divide-gray-200">
+                      <li className="py-4 flex md:py-0 md:pb-4">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          Unlimited Platforms and Databases
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          Unlimited Program executions
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          1 program development per month
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          Unlimited notifications and alerts
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          Free monitoring and maintenance
+                        </span>
+                      </li>
+                    </ul>
+                    <ul className="border-t border-gray-200 divide-y divide-gray-200 md:border-t-0">
+                      <li className="py-4 flex md:border-t-0 md:py-0 md:pb-4">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          1 hour/month zoom with Founder/CEO
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          Access to Free Program Marketplace
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          Unlimited Users
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          1 email, web, sms, video template / month
+                        </span>
+                      </li>
+                      <li className="py-4 flex">
+                        {/* Heroicon name: outline/check */}
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-green-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span className="ml-3 text-base text-gray-500">
+                          Excelent service aimed at long term contracts
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             {/* CTA Section */}
             <div className="relative bg-gray-900">
-              <div className="relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+              <div className="relative h-56 bg-green-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
                 <img
                   className="w-full h-full object-cover"
                   src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixqx=1JO2mPl47d&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100"
