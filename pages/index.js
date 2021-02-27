0ee76a03abd462c2d0cf03a54c1ed4dca3c70b7d
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-27 07:57:43 -0600

    brands

--- pages/index.js
+++ pages/index.js
@@ -71,44 +71,6 @@ export default function Home() {
                       <p className="mt-3 text-2xl text-gray-300 sm:mt-7 sm:text-2xl lg:text-3xl xl:text-3xl">
                         Order a JD program to make it possible!
                       </p>
-
-                      <div className="mt-10 sm:mt-12">
-                        <form className="sm:max-w-xl sm:mx-auto lg:mx-0">
-                          <div className="sm:flex">
-                            <div className="min-w-0 flex-1">
-                              <label htmlFor="email" className="sr-only">
-                                Tell us your idea
-                              </label>
-                              <textarea
-                                value={phone}
-                                onChange={(e) =>
-                                  setPhone(e.currentTarget.value)
-                                }
-                                type="number"
-                                placeholder="Tell use your idea"
-                                className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-                              ></textarea>
-                              <p className="mt-3 text-sm text-gray-300 sm:mt-4">
-                                A JD Program is able to do tasks with data,
-                                platforms and appliances that are connected to
-                                the internet.
-                              </p>
-                            </div>
-                            <div className="mt-3 sm:mt-0 sm:ml-3">
-                              <a
-                                onClick={onCreateAccount}
-                                className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-                              >
-                                Start
-                              </a>
-                            </div>
-                          </div>
-
-                          <p className="mt-3 text-md text-red-800 sm:mt-4">
-                            {error}
-                          </p>
-                        </form>
-                      </div>
                     </div>
                   </div>
                   <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
