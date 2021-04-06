d9121fe8344e83261efc74af8e79b6b54ec6483f
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-04-05 21:08:46 -0600

    f

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,7 @@
-import { DefaultSeo } from "next-seo";
-import App, { Container } from "next/app";
-
 import "../styles/globals.css";
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <Container>
-        <DefaultSeo
-          openGraph={{
-            title="JungleDynamics",
-            description="Customer Data Platform, Custom built by Roberto Rodriguez for each customer or white-labeled for trusted partners",
-            type: "website",
-            locale: "en_US",
-            url: "https://jungledynamics.com/",
-            site_name: "Jungledynamics",
-            images: [
-              {
-                url:
-                  "https://jungledynamics.com/color1_logo_transparent_background.png",
-              },
-            ],
-          }}
-        />
-        <Component {...pageProps} />
-      </Container>
-    );
-  }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
