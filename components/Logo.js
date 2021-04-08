630c90c45815cb2654f512fb70d0547e177cc116
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-04-08 17:15:07 -0600

    f

--- components/Logo.js
+++ components/Logo.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function Logo() {
-  const [logo, setLogo] = React.useState("jd");
+  const [logo, setLogo] = React.useState("JungleDynamics");
 
   React.useEffect(() => {
     if (window.location.host.indexOf("localhost") > -1) setLogo("Mogi");
-    else if (window.location.host.indexOf("jungledynamics") > -1) setLogo("JD");
+    else if (window.location.host.indexOf("jungledynamics") > -1)
+      setLogo("JungleDynamics");
     else if (window.location.host.indexOf("mogi") > -1) setLogo("Mogi`");
     else if (window.location.host.indexOf("rodco") > -1) setLogo("Rodco`");
     else if (window.location.host.indexOf("signalon") > -1) setLogo("Signalon");
